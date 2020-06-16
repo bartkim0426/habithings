916bed979bb2a16e2420753b00a2a63d0b5c1ef4
seulchankim <bartkim0426@gmail.com>
2020-06-16 23:19:33 +0900

    Refactoring to class to ThingsDB, HabiThings

--- habitica.py
+++ habitica.py
@@ -2,7 +2,7 @@ import os
 import time
 import json
 import sqlite3
-from datetime import datetime, date, timedelta
+from datetime import datetime, date
 from dataclasses import dataclass
 
 import httpx
@@ -18,72 +18,6 @@ HABITICA_HEADERS = {
     "Content-Type": "application/json",
 }
 
-def execute_things_sqilte3(query):
-    # TODO: check sql file is exist first
-    print(THINGS_DB)
-    conn = sqlite3.connect(THINGS_DB)
-    cur = conn.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
-    return rows
-
-
-# def select_all_things_24_hours():
-#     query = '''
-#     SELECT title, uuid
-#     FROM TMTask
-#     WHERE trashed = 0 AND type = 0
-#     AND status = 3
-#     AND stopDate > strftime('%s','now','-24 hours');
-#     '''
-#     data = execute_things_sqilte3(query)
-#     return data
-
-
-def get_task_id(data: dict):
-    try:
-        return data["data"]["_id"]
-    except KeyError:
-        return None
-
-
-def create_habitica_task(content: str, things_uuid: str):
-    url = "https://habitica.com/api/v3/tasks/user"
-    data = {
-        "text": content,
-        "type": "todo",
-        "alias": f'task-from-things-{things_uuid}',
-        "priority": 0.1, # 0.1, 1, 1.5, 2
-    }
-    res = httpx.post(url, data=json.dumps(data), headers=HABITICA_HEADERS)
-    return res
-
-
-def score_habitica_task_from_id(task_id: str):
-    url = f'https://habitica.com/api/v3/tasks/{task_id}/score/up'
-    res = httpx.post(url, headers=HABITICA_HEADERS)
-    return res
-
-
-# def create_habitica_task_from_things() -> list:
-#     things_rows = select_all_things_24_hours()
-#     responses = []
-#     for row in things_rows:
-#         res = create_habitica_task(row[0])
-#         responses.append(res)
-#     return responses
-
-
-def get_task_id_from_response(create_response):
-    try:
-        assert create_response.status_code == 201
-        result = create_response.json()
-        assert result['success'] == True
-    except AssertionError:
-        print(create_response.json())
-        return ''
-    return result['data']['id']
-
 
 @dataclass
 class TaskResult:
@@ -91,56 +25,114 @@ class TaskResult:
     task_id: str
     content: str
     is_success: bool
-#     def print_total_result(self) -> str:
-#         return f"{count(is_success}"
 
-def mktime_from_string(date_str):
-    return time.mktime(datetime.strptime(date_str, "%Y-%m-%d").timetuple())
 
-def select_things_by_date(start_date: str, end_date=date.today().strftime('%Y-%m-%d')):
+class ThingsDB:
     '''
-    start_time: %Y-%m-%d
-    end_time: %Y-%m-%d
+    DB initialize and execute Things3 SQLite3 database
     '''
-    # TODO: add validation for date format
-    start_timestamp = time.mktime(datetime.strptime(start_date, "%Y-%m-%d").timetuple())
-    end_timestamp = time.mktime(datetime.strptime(end_date, "%Y-%m-%d").timetuple())
-    query = f'''
-    SELECT title, uuid
-    FROM TMTask
-    WHERE trashed = 0 AND type = 0
-    AND status = 3
-    AND stopDate >= {start_timestamp}
-    ANd stopDate < {end_timestamp}
+    def __init__(self, database=THINGS_DB):
+        self.database = database
+        self.connection = None
+        self.connected = False
+
+    def connect(self):
+        '''Connect to the SQLite3 database'''
+        self.connection = sqlite3.connect(self.database)
+        self.cursor = self.connection.cursor()
+        self.connected = True
+
+    def close(self):
+        if not self.connection:
+            return
+        '''Close the SQLite3 database'''
+        self.connection.commit()
+        self.connection.close()
+        self.connected = False
+
+    def execute(self, query):
+        # TODO: validation for only select
+        self.cursor.execute(query)
+
+    def get_rows(self, query):
+        self.execute(query)
+        return self.cursor.fetchall()
+
+    def get_complete_tasks_between(self, start_date, end_date):
+        start_timestamp = time.mktime(datetime.strptime(start_date, "%Y-%m-%d").timetuple())
+        end_timestamp = time.mktime(datetime.strptime(end_date, "%Y-%m-%d").timetuple())
+        query = f'''
+        SELECT title, uuid
+        FROM TMTask
+        WHERE trashed = 0 AND type = 0
+        AND status = 3
+        AND stopDate >= {start_timestamp}
+        ANd stopDate < {end_timestamp}
+        '''
+        return self.get_rows(query)
+
+
+class HabiThings:
     '''
-    data = execute_things_sqilte3(query)
-    return data
+    Create and score Habitica task from Things 3 app sqilte db
 
+    execute_things_sqlite3
+    '''
+    def __init__(self, db_path=THINGS_DB):
+        self.db = ThingsDB(database=db_path)
+        self.db.connect()
 
-def create_and_score(things_rows: list) -> list:
-    # TODO: change results to namedtuple
-    results = []
-    for row in things_rows:
-        content, things_uuid = row[0:2]
-        create_res = create_habitica_task(content, things_uuid)
-        task_id = get_task_id_from_response(create_res)
-        if task_id:
-            score_res = score_habitica_task_from_id(task_id)
-            is_score_success = score_res.json().get('success', False)
-        else:
-            is_score_success = False
-        results.append(
-            TaskResult(task_id, content, is_score_success)
-        )
-    return results
+    def select_things_by_date(self, start_date: str, end_date=date.today().strftime('%Y-%m-%d')):
+        '''
+        start_time: %Y-%m-%d
+        end_time: %Y-%m-%d
+        '''
+        return self.db.get_complete_tasks_between(start_date, end_date)
 
+    def create_habitica_task(self, content: str, things_uuid: str):
+        url = "https://habitica.com/api/v3/tasks/user"
+        data = {
+            "text": content,
+            "type": "todo",
+            "alias": f'task-from-things-{things_uuid}',
+            "priority": 0.1,  # 0.1, 1, 1.5, 2
+        }
+        res = httpx.post(url, data=json.dumps(data), headers=HABITICA_HEADERS)
+        return res
 
-def create_and_score_by_date(start_date: str, end_date=date.today().strftime('%Y-%m-%d')) -> list:
-    things_rows = select_things_by_date(start_date, end_date)
-    return create_and_score(things_rows)
+    def score_habitica_task_from_id(self, task_id: str):
+        url = f'https://habitica.com/api/v3/tasks/{task_id}/score/up'
+        res = httpx.post(url, headers=HABITICA_HEADERS)
+        return res
 
+    def get_task_id_from_response(self, create_response):
+        try:
+            assert create_response.status_code == 201
+            result = create_response.json()
+            assert result['success'] is True
+        except AssertionError:
+            # TODO: add logging
+            print(create_response.json())
+            return ''
+        return result['data']['id']
 
-def daily_create_and_score():
-    yesterday = datetime.strftime(date.today() - timedelta(days=1), '%Y-%m-%d')
-    things_rows = select_things_by_date(yesterday)
-    return create_and_score(things_rows)
+    def create_and_score(self, things_rows: list) -> list:
+        # TODO: change results to namedtuple
+        results = []
+        for row in things_rows:
+            content, things_uuid = row[0:2]
+            create_res = self.create_habitica_task(content, things_uuid)
+            task_id = self.get_task_id_from_response(create_res)
+            if task_id:
+                score_res = self.score_habitica_task_from_id(task_id)
+                is_score_success = score_res.json().get('success', False)
+            else:
+                is_score_success = False
+            results.append(
+                TaskResult(task_id, content, is_score_success)
+            )
+        return results
+
+    def create_and_score_by_date(self, start_date: str, end_date=date.today().strftime('%Y-%m-%d')) -> list:
+        things_rows = self.select_things_by_date(start_date, end_date)
+        return self.create_and_score(things_rows)
--- tests.py
+++ tests.py
@@ -1,12 +1,14 @@
 import os
 import json
 import unittest
+import sqlite3
 from unittest import TestCase
 from unittest.mock import patch
 
 import habitica
 
 FAKE_DATA = os.path.join(os.path.dirname(__file__), 'fake-results.json')
+
 
 class MockResponse:
     def __init__(self, json_data, status_code):
@@ -16,13 +18,70 @@ class MockResponse:
     def json(self):
         return self.json_data
 
-class ThingsHabiticaTestCase(TestCase):
-    def setUp(self):
-       fake_data = json.loads(open(FAKE_DATA).read())
-       self.create_success_data = fake_data.get('create_success_data')
-       self.score_success_data = fake_data.get('score_success_data')
 
-    @patch('habitica.create_habitica_task')
+class ThingsDBTestCase(TestCase):
+    @staticmethod
+    def things_db():
+        db = habitica.ThingsDB(database='./test.sqlite3')
+        return db
+
+    # def setUp(self):
+        # self.db = habitica.ThingsDB()
+
+    def test_connect(self):
+        db = self.things_db()
+        db.connect()
+        self.assertIsInstance(db.cursor, sqlite3.Cursor)
+        self.assertTrue(db.connected)
+
+    def test_close_without_connect(self):
+        db = self.things_db()
+        db.close()
+        self.assertFalse(db.connected)
+
+    def test_close(self):
+        db = self.things_db()
+        db.connect()
+        db.close()
+        self.assertFalse(db.connected)
+
+    def test_execute(self):
+        db = self.things_db()
+        db.connect()
+        db.execute('select 1')
+        self.assertTrue(db.connected)
+
+    def test_get_rows(self):
+        db = self.things_db()
+        db.connect()
+        result = db.get_rows('select 1')
+
+        self.assertEqual(result, [(1,)])
+
+    def test_get_complete_tasks_between(self):
+        db = self.things_db()
+        db.connect()
+        start, end = '2020-01-01', '2020-06-10'
+        result = db.get_complete_tasks_between(start, end)
+        self.assertEqual(len(result), 6)
+
+
+class HabiThingsTestCase(TestCase):
+    def setUp(self):
+        self.ht = habitica.HabiThings(db_path='./test.sqlite3')
+        fake_data = json.loads(open(FAKE_DATA).read())
+        self.create_success_data = fake_data.get('create_success_data')
+        self.score_success_data = fake_data.get('score_success_data')
+
+    def test_habi_things_class_init(self):
+        self.assertTrue(self.ht.db.connected)
+
+    def test_select_things_by_date(self):
+        start, end = '2020-01-01', '2020-06-10'
+        result = self.ht.select_things_by_date(start, end)
+        self.assertEqual(len(result), 6)
+
+    @patch('habitica.HabiThings.create_habitica_task')
     def test_create_task(self, mock_create):
         mock_create.return_value = MockResponse(
             status_code=201,
@@ -30,50 +89,20 @@ class ThingsHabiticaTestCase(TestCase):
         )
         content = 'test content'
         things_uuid = '3C162AC4-4629-489B-8587-C51E9C8A1A91'
-        res = habitica.create_habitica_task(content, things_uuid)
+        res = self.ht.create_habitica_task(content, things_uuid)
+
         self.assertEqual(res.status_code, 201)
         self.assertEqual(res.json(), self.create_success_data)
 
-    @patch('habitica.create_habitica_task')
-    def test_get_task_id(self, mock_create):
-        mock_create.return_value = MockResponse(
-            status_code=201,
-            json_data=self.create_success_data,
-        )
-        res = habitica.create_habitica_task('test')
-        data = res.json()
-        result = habitica.get_task_id(data)
-        self.assertEqual(result, "1fdf8c4a-00dc-48ac-97fb-cedcdd041560")
-
-    def test_execute_things_sqlite3(self):
-        result = habitica.execute_things_sqilte3('select 1')
-        self.assertEqual(result, [(1, )])
-        
-#     # TODO: mock Things.sqlite3 data from others
-#     # TODO: mock Things.sqlite3 data -> 24 hour invalid for the days
-#     @patch('habitica.THINGS_DB', './Things.sqlite3')
-#     def test_select_all_things_tasks_24_hours(self):
-#         result = habitica.select_all_things_24_hours()
-#         self.assertEqual(len(result), 1)
-# 
-  # TODO: mock Things.sqlite3 data -> 24 hour invalid for the days
-#     @patch('habitica.create_habitica_task')
-#     def test_create_habitica_task_from_things(self, mock_create):
-#         mock_create.return_value = MockResponse(
-#             status_code=201,
-#             json_data=self.create_success_data,
-#         )
-#         result = habitica.create_habitica_task_from_things()
-#         self.assertEqual(len(result), 9)
-
-    @patch('habitica.score_habitica_task_from_id')
+    @patch('habitica.HabiThings.score_habitica_task_from_id')
     def test_score_habitica_task_from_id(self, mock_score):
         mock_score.return_value = MockResponse(
             status_code=200,
             json_data=self.score_success_data,
         )
         task_id = "1fdf8c4a-00dc-48ac-97fb-cedcdd041560"
-        res = habitica.score_habitica_task_from_id(task_id)
+        res = self.ht.score_habitica_task_from_id(task_id)
+
         self.assertEqual(res.status_code, 200)
 
     def test_get_task_id_from_response(self):
@@ -81,51 +110,12 @@ class ThingsHabiticaTestCase(TestCase):
             status_code=201,
             json_data=self.create_success_data,
         )
-        result = habitica.get_task_id_from_response(res)
+        result = self.ht.get_task_id_from_response(res)
+
         self.assertEqual(result, "1fdf8c4a-00dc-48ac-97fb-cedcdd041560")
 
-    @patch('habitica.score_habitica_task_from_id')
-    @patch('habitica.create_habitica_task')
-    @patch('habitica.THINGS_DB', './Things.sqlite3')
-    def test_daily_create_and_score(self, mock_create, mock_score):
-        mock_score.return_value = MockResponse(
-            status_code=200,
-            json_data=self.score_success_data,
-        )
-        mock_create.return_value = MockResponse(
-            status_code=201,
-            json_data=self.create_success_data,
-        )
-        result = habitica.daily_create_and_score()
-        self.assertEqual(len(result), 0)
-        # self.assertTrue(result[0].is_success)
-
-    @patch('habitica.THINGS_DB', './Things.sqlite3')
-    def test_select_things_by_date(self):
-        start, end = '2020-01-01', '2020-06-10'
-        result = habitica.select_things_by_date(start, end)
-        self.assertEqual(len(result), 6)
-
-    # TODO: select_things_by_date를 사용해서 habitica에 api 전송할 수 있게 (날짜 선택해서 해당 날짜만 create and score)
-    @patch('habitica.score_habitica_task_from_id')
-    @patch('habitica.create_habitica_task')
-    @patch('habitica.THINGS_DB', './Things.sqlite3')
-    def test_create_and_score_by_date(self, mock_create, mock_score):
-        mock_score.return_value = MockResponse(
-            status_code=200,
-            json_data=self.score_success_data,
-        )
-        mock_create.return_value = MockResponse(
-            status_code=201,
-            json_data=self.create_success_data,
-        )
-        start, end = '2020-01-01', '2020-06-10'
-        result = habitica.create_and_score_by_date(start, end)
-        self.assertEqual(len(result), 6)
-
-    @patch('habitica.score_habitica_task_from_id')
-    @patch('habitica.create_habitica_task')
-    @patch('habitica.THINGS_DB', './Things.sqlite3')
+    @patch('habitica.HabiThings.score_habitica_task_from_id')
+    @patch('habitica.HabiThings.create_habitica_task')
     def test_create_and_score(self, mock_create, mock_score):
         mock_score.return_value = MockResponse(
             status_code=200,
@@ -136,13 +126,25 @@ class ThingsHabiticaTestCase(TestCase):
             json_data=self.create_success_data,
         )
         start, end = '2020-01-01', '2020-06-10'
-        rows = habitica.select_things_by_date(start, end)
-        result = habitica.create_and_score(rows)
+        rows = self.ht.select_things_by_date(start, end)
+        result = self.ht.create_and_score(rows)
+
         self.assertEqual(len(result), 6)
 
-    # TODO: 일단정리해서 깃헙에 올리기
-    # TODO: class refactoring
-    # TODO: 혹은 이미 추가된 항목 validation
+    @patch('habitica.HabiThings.score_habitica_task_from_id')
+    @patch('habitica.HabiThings.create_habitica_task')
+    def test_create_and_score_by_date(self, mock_create, mock_score):
+        mock_score.return_value = MockResponse(
+            status_code=200,
+            json_data=self.score_success_data,
+        )
+        mock_create.return_value = MockResponse(
+            status_code=201,
+            json_data=self.create_success_data,
+        )
+        start, end = '2020-01-01', '2020-06-10'
+        result = self.ht.create_and_score_by_date(start, end)
+        self.assertEqual(len(result), 6)
 
 
 if __name__ == '__main__':
